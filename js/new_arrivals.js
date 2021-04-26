187d8e9b38531322ef85a2b1d4c6f6972178c77e
lelekaspb <bov-1986@yandex.ru>
2021-04-26 23:43:38 +0200

    worked on colors filter

--- js/new_arrivals.js
+++ js/new_arrivals.js
@@ -11,15 +11,14 @@ document
   .querySelector("label.collection .chevron")
   .addEventListener("click", closeFilterSection);
 
-//select#color.select_hidden
-
 function closeFilterSection() {
   console.log(this.dataset.id);
   const filterID = this.dataset.id;
-  document
-    .querySelector("select#" + filterID)
-    .classList.toggle("select_hidden");
-  // this.src = "../assets/images/icons/chevron-up.svg";
+  document.querySelector("#" + filterID).classList.toggle("select_hidden");
   this.classList.toggle("open");
   console.log(this);
 }
+
+// document.querySelector("input.filter_submit").addEventListener("click", (e) => {
+//   e.preventDefault();
+// });
