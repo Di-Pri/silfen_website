40024edeae54f9a3ad46799319017191986323ff
lelekaspb <bov-1986@yandex.ru>
2021-04-28 15:47:06 +0200

    done with the mobile filter

--- js/new_arrivals.js
+++ js/new_arrivals.js
@@ -19,7 +19,6 @@ function openFilterSection() {
   console.log(this);
 }
 
-
 // document.querySelector("#filter_form").addEventListener('submit', applyFilter);
 
 // function applyFilter(e) {
@@ -28,79 +27,90 @@ function openFilterSection() {
 
 // slider functionality
 var inputLeft = document.getElementById("input-left");
-      var inputRight = document.getElementById("input-right");
+var inputRight = document.getElementById("input-right");
 
-      var thumbLeft = document.querySelector(".slider > .thumb.left");
-      var thumbRight = document.querySelector(".slider > .thumb.right");
-      var range = document.querySelector(".slider > .range");
+var thumbLeft = document.querySelector(".slider > .thumb.left");
+var thumbRight = document.querySelector(".slider > .thumb.right");
+var range = document.querySelector(".slider > .range");
 
-      function setLeftValue() {
-        var _this = inputLeft,
-          min = parseInt(_this.min),
-          max = parseInt(_this.max);
+function setLeftValue() {
+  var _this = inputLeft,
+    min = parseInt(_this.min),
+    max = parseInt(_this.max);
 
-        _this.value = Math.min(
-          parseInt(_this.value),
-          parseInt(inputRight.value) - 1
-        );
-        console.log(_this.value);
-        document.querySelector("label.label-left > span").textContent =
-          _this.value;
-        var percent = ((_this.value - min) / (max - min)) * 100;
+  _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
+  console.log(_this.value);
+  document.querySelector("label.label-left > span").textContent = _this.value;
+  var percent = ((_this.value - min) / (max - min)) * 100;
 
-        thumbLeft.style.left = percent + "%";
-        range.style.left = percent + "%";
-        //console.log(percent);
-      }
-      setLeftValue();
+  thumbLeft.style.left = percent + "%";
+  range.style.left = percent + "%";
+  //console.log(percent);
+}
+setLeftValue();
 
-      function setRightValue() {
-        var _this = inputRight,
-          min = parseInt(_this.min),
-          max = parseInt(_this.max);
+function setRightValue() {
+  var _this = inputRight,
+    min = parseInt(_this.min),
+    max = parseInt(_this.max);
 
-        _this.value = Math.max(
-          parseInt(_this.value),
-          parseInt(inputLeft.value) + 1
-        );
-        console.log(_this.value);
-        document.querySelector(
-          "label.label-right > span"
-        ).textContent = _this.value;
-        var percent = ((_this.value - min) / (max - min)) * 100;
+  _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
+  console.log(_this.value);
+  document.querySelector("label.label-right > span").textContent = _this.value;
+  var percent = ((_this.value - min) / (max - min)) * 100;
 
-        thumbRight.style.right = 100 - percent + "%";
-        range.style.right = 100 - percent + "%";
-      }
-      setRightValue();
+  thumbRight.style.right = 100 - percent + "%";
+  range.style.right = 100 - percent + "%";
+}
+setRightValue();
 
-      inputLeft.addEventListener("input", setLeftValue);
-      inputRight.addEventListener("input", setRightValue);
+inputLeft.addEventListener("input", setLeftValue);
+inputRight.addEventListener("input", setRightValue);
 
-      inputLeft.addEventListener("mouseover", function () {
-        thumbLeft.classList.add("hover");
-      });
-      inputLeft.addEventListener("mouseout", function () {
-        thumbLeft.classList.remove("hover");
-      });
-      inputLeft.addEventListener("mousedown", function () {
-        thumbLeft.classList.add("active");
-      });
-      inputLeft.addEventListener("mouseup", function () {
-        thumbLeft.classList.remove("active");
-      });
+inputLeft.addEventListener("mouseover", function () {
+  thumbLeft.classList.add("hover");
+});
+inputLeft.addEventListener("mouseout", function () {
+  thumbLeft.classList.remove("hover");
+});
+inputLeft.addEventListener("mousedown", function () {
+  thumbLeft.classList.add("active");
+});
+inputLeft.addEventListener("mouseup", function () {
+  thumbLeft.classList.remove("active");
+});
 
-      inputRight.addEventListener("mouseover", function () {
-        thumbRight.classList.add("hover");
-      });
-      inputRight.addEventListener("mouseout", function () {
-        thumbRight.classList.remove("hover");
-      });
-      inputRight.addEventListener("mousedown", function () {
-        thumbRight.classList.add("active");
-      });
-      inputRight.addEventListener("mouseup", function () {
-        thumbRight.classList.remove("active");
-      });
+inputRight.addEventListener("mouseover", function () {
+  thumbRight.classList.add("hover");
+});
+inputRight.addEventListener("mouseout", function () {
+  thumbRight.classList.remove("hover");
+});
+inputRight.addEventListener("mousedown", function () {
+  thumbRight.classList.add("active");
+});
+inputRight.addEventListener("mouseup", function () {
+  thumbRight.classList.remove("active");
+});
 
 // slider functionality end
+
+// mobile filter
+const filterCloseBtn = document.querySelector(".filter_close_btn");
+const filterOpenBtn = document.querySelector(".filter_open_btn");
+console.log(filterOpenBtn);
+
+filterCloseBtn.addEventListener("click", closeMobileFilter);
+filterOpenBtn.addEventListener("click", openFilterMobile);
+
+function closeMobileFilter() {
+  document.querySelector(".filter_mobile").classList.remove("opened");
+  document.querySelector(".filter_mobile").classList.add("closed");
+  window.scrollTo(0, 0);
+}
+
+function openFilterMobile() {
+  document.querySelector(".filter_mobile").classList.remove("closed");
+  document.querySelector(".filter_mobile").classList.add("opened");
+  console.log(filterOpenBtn + "was clicked");
+}
