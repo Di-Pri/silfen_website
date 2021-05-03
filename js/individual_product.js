08ceea4f755bfe6a2ee64f24e2e7aa884ae7acc8
lelekaspb <bov-1986@yandex.ru>
2021-05-03 17:28:42 +0200

    working on individual page

--- js/individual_product.js
+++ js/individual_product.js
@@ -1,3 +1,53 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("product");
+console.log(id);
+
+const url =
+  "https://kea0209-5a57.restdb.io/rest/products/" + id + "?fetchchildren=true";
+
+function getData() {
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "x-apikey": "6082d28c28bf9b609975a5db",
+    },
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      //console.log(response);
+      showProduct(response);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+getData();
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector("title").textContent =
+    product.title.charAt(0).toUpperCase() + product.title.slice(1);
+  document.querySelector(".product-description h1").textContent = product.title;
+  if (product.sale) {
+    document.querySelector(".price_current_number").textContent =
+      product.price_current;
+    document.querySelector(".price_regular_number").textContent =
+      product.price_regular;
+  } else {
+    document.querySelector(".price_current").style.display = "none";
+    document.querySelector(".price_regular").style.textDecoration = "none";
+  }
+  document.querySelector("#parag1").textContent = product.description;
+  //console.log(document.querySelector("#parag1"));
+  document.querySelector("#parag2").innerHTML = product.details;
+  product.primary_images.forEach((elem) => {
+    console.log(elem);
+    const colorpickerTemplate = document.querySelector(".colorpicker_images")
+      .content;
+  });
+}
+
 const parag1 = document.querySelector("#parag1");
 const parag2 = document.querySelector("#parag2");
 const parag3 = document.querySelector("#parag3");
